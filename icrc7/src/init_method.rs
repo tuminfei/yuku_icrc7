ea068fb90ef95a3a77d3c674dd5370ad2e65325f
Terry.Tu <tuminfei1981@gmail.com>
2024-03-16 10:13:10 +0800

    add pre_upgrade

--- icrc7/src/init_method.rs
+++ icrc7/src/init_method.rs
@@ -1,4 +1,5 @@
-use ic_cdk_macros::init;
+use ic_cdk_macros::{init, post_upgrade, pre_upgrade};
+use ic_stable_structures::{writer::Writer, Memory};
 use icrc_ledger_types::icrc1::account::Account;
 
 use crate::{icrc7_types::InitArg, state::STATE, utils::account_transformer};
@@ -32,4 +33,41 @@ pub fn init(arg: InitArg) {
         s.tx_window = arg.tx_window;
         s.permitted_drift = arg.permitted_drift;
     })
+}
+
+#[pre_upgrade]
+fn pre_upgrade() {
+    // Serialize the state.
+    // This example is using CBOR, but you can use any data format you like.
+    let mut state_bytes = vec![];
+    STATE
+        .with(|s| ciborium::ser::into_writer(&*s.borrow(), &mut state_bytes))
+        .expect("failed to encode state");
+
+    // Write the length of the serialized bytes to memory, followed by the
+    // by the bytes themselves.
+    let len = state_bytes.len() as u32;
+    let mut memory = crate::memory::get_upgrades_memory();
+    let mut writer = Writer::new(&mut memory, 0);
+    writer.write(&len.to_le_bytes()).unwrap();
+    writer.write(&state_bytes).unwrap();
+}
+
+// A post-upgrade hook for deserializing the data back into the heap.
+#[post_upgrade]
+fn post_upgrade() {
+    let memory = crate::memory::get_upgrades_memory();
+
+    // Read the length of the state bytes.
+    let mut state_len_bytes = [0; 4];
+    memory.read(0, &mut state_len_bytes);
+    let state_len = u32::from_le_bytes(state_len_bytes) as usize;
+
+    // Read the bytes
+    let mut state_bytes = vec![0; state_len];
+    memory.read(4, &mut state_bytes);
+
+    // Deserialize and set the state.
+    let state = ciborium::de::from_reader(&*state_bytes).expect("failed to decode state");
+    STATE.with(|s| *s.borrow_mut() = state);
 }
